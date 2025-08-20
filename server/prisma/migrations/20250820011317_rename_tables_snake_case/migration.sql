8e3d8ce4429c66af6d9e998b459c8de1b8633cf1
Matthew Fehr <mattfehr2004@gmail.com>
2025-08-19 18:21:04 -0700

    add mapping to schema to go between postgres convention and ts

---
+++ server/prisma/migrations/20250820011317_rename_tables_snake_case/migration.sql
@@ -0,0 +1,162 @@
+-- CreateEnum
+CREATE TYPE "public"."ProductType" AS ENUM ('tower', 'module', 'addon');
+
+-- CreateEnum
+CREATE TYPE "public"."QuestionType" AS ENUM ('question', 'feedback');
+
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "user_id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."products" (
+    "product_id" TEXT NOT NULL,
+    "product_type" "public"."ProductType" NOT NULL,
+    "creator_id" TEXT,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "description" TEXT,
+    "is_public" BOOLEAN NOT NULL DEFAULT true,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "sensor_pin" TEXT,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("product_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."tower_components" (
+    "tower_id" TEXT NOT NULL,
+    "part_id" TEXT NOT NULL,
+    "position" INTEGER,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "tower_components_pkey" PRIMARY KEY ("tower_id","part_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."cart_items" (
+    "user_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "cart_items_pkey" PRIMARY KEY ("user_id","product_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."favorites" (
+    "user_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+
+    CONSTRAINT "favorites_pkey" PRIMARY KEY ("user_id","product_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."orders" (
+    "order_number" TEXT NOT NULL,
+    "customer_id" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "total_price" DOUBLE PRECISION NOT NULL,
+    "purchase_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("order_number")
+);
+
+-- CreateTable
+CREATE TABLE "public"."order_items" (
+    "order_number" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price_at_purchase" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "order_items_pkey" PRIMARY KEY ("order_number","product_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."comments" (
+    "user_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "post_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("user_id","product_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."follows" (
+    "follower_id" TEXT NOT NULL,
+    "following_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "follows_pkey" PRIMARY KEY ("follower_id","following_id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."questions" (
+    "question_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "question_type" "public"."QuestionType" NOT NULL,
+    "content" TEXT NOT NULL,
+    "response" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("question_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "public"."users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."products" ADD CONSTRAINT "products_creator_id_fkey" FOREIGN KEY ("creator_id") REFERENCES "public"."users"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."tower_components" ADD CONSTRAINT "tower_components_tower_id_fkey" FOREIGN KEY ("tower_id") REFERENCES "public"."products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."tower_components" ADD CONSTRAINT "tower_components_part_id_fkey" FOREIGN KEY ("part_id") REFERENCES "public"."products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."cart_items" ADD CONSTRAINT "cart_items_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."cart_items" ADD CONSTRAINT "cart_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."favorites" ADD CONSTRAINT "favorites_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."favorites" ADD CONSTRAINT "favorites_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."orders" ADD CONSTRAINT "orders_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."order_items" ADD CONSTRAINT "order_items_order_number_fkey" FOREIGN KEY ("order_number") REFERENCES "public"."orders"("order_number") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."order_items" ADD CONSTRAINT "order_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."comments" ADD CONSTRAINT "comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."comments" ADD CONSTRAINT "comments_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "public"."products"("product_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."follows" ADD CONSTRAINT "follows_follower_id_fkey" FOREIGN KEY ("follower_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."follows" ADD CONSTRAINT "follows_following_id_fkey" FOREIGN KEY ("following_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."questions" ADD CONSTRAINT "questions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
